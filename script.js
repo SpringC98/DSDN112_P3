007b3ca8c8dd9746e36d795fb48d456725ac661a
SpringC98 <springcrai@myvuw.ac.nz>
2018-10-02 12:22:08 +1300

    Commit 5: Restructured JS rotation (one elem only) + HTML Structuring

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-var element = document.getElementById(elem);
+// Keep a permanent record of how many degrees we have rotated
+var rotation = 0;
 
-element.addEventListener('wheel', rotateSelection, false);
+// Why this? Well you're not guaranteed the "cog" element exists until the page is finished loading the DOM
+document.addEventListener("DOMContentLoaded", function() {
+	// Get the cog once, not every time we rotate
+	var element = document.getElementById("cog");
 
-function rotateSelection(element){
-    if(element.deltaY < 0){
-        element.style.transform = "rotate(45deg)";
-    }
-    if(element.deltaY > 0){
-      element.style.transform = "rotate(-45deg)";
-    }
+	// On wheel, run our function. Pass the element + the event
+	element.addEventListener("wheel", function(evt) { rotateSelection(element, evt); }, false);
+});
+
+function rotateSelection(element, evt) {
+	// DeltaY provides how much the user has scrolled (1 wheel = 100 units)
+	// Divide by 10 because 100 is too much
+	rotation = (rotation + evt.deltaY / 10);
+	element.style.transform = "rotate(" + rotation + "deg)";
 }
