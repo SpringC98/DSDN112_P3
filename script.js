6ce4d43cbbce91be29771a0bfd93f33b08912c2a
Lucas Savva <lucas@m1cr0man.com>
2018-10-15 23:03:34 +0100

    Add emote match checking

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-var emoteCheck = [];
-
 function setCog(cogId){
-	var rotation = 0;
-	var numberOfEmotes = 0;
-	var emote;
-
 	var cog = document.getElementById(cogId);
 
 	rotateMemes(cog);
@@ -13,10 +7,12 @@ function setCog(cogId){
 	cog.addEventListener("wheel", function(evt){
 		// DeltaY provides how much the user has scrolled (1 wheel = 100 units)
 		// Divide by 10 because 100 is too much
-		rotation = (rotation + evt.deltaY / 10);
-		emote = cog.children[rotationCheck(cog, rotation, numberOfEmotes)];
+		var rotation = ((+cog.getAttribute("data-rotation")) + evt.deltaY / 10);
+		cog.setAttribute("data-rotation", '' + rotation);
 		cog.style.transform = "rotate(" + rotation + "deg)";
 	}, false);
+
+	return cog;
 }
 
 // Rotates elements inside cog evenly depending on how many there is
@@ -28,18 +24,36 @@ function rotateMemes(cog){
 	}
 }
 
-function rotationCheck(cog, rotation, counter){
-	var cogAngle = Math.round(((rotation)%360)/cog.children.length);
-	cogAngle = cogAngle/10;
-
-	if(cogAngle==0){
-		counter++;
-		if(counter>(cog.children.length-1)){
-			counter=0;
-		}
-		return counter;
+function getEmote(cog) {
+	var rotation = +cog.getAttribute("data-rotation");
+	var pos = Math.round((Math.abs(rotation)%360) / (360 / cog.children.length));
+	var index = 0;
+	// Press F to figure this out
+	if (pos == 0 || pos == 6) {
+		// Fffffffffffffffffff
+		index = 0;
+	} else if (rotation < 0 && pos > 0) {
+		index = pos;
+	} else {
+		index = cog.children.length - pos;
 	}
-	return counter;
+
+	return cog.children[index].firstElementChild.attributes["data-emote"].value;
+}
+
+function checkEmotes(cogs){
+	// Map -> Takes items in a list and applies a function to them.
+	// The return value becomes the new value in the list
+	// Set -> List of unique items
+	// So, if all the emotes are the same the length of this set will be 1
+	// Oh yeah and length of Set != length, it's size. MonkaS
+	var same = (new Set(cogs.map(getEmote))).size == 1
+
+	if (same) {
+		alert("You're correct! PogChamp");
+	} else {
+		alert("You're WRONG! MonkaS");
+	}
 }
 
 function on() {
@@ -53,4 +67,8 @@ function off() {
 document.addEventListener("DOMContentLoaded", function(){
 	var cog1 = setCog("cog_left");
 	var cog2 = setCog("cog_right");
+
+	document.getElementById("emote_check").onclick = function() {
+		checkEmotes([cog1, cog2]);
+	}
 });
